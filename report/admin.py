3321f194ec4e7d710e6e0b39f3886d9cd392f624
Tirkishmammet Annamammedov <tirkishdeveloper@gmail.com>
2023-05-12 20:22:16 +0500

    commit 12/05/2023

--- report/admin.py
+++ report/admin.py
@@ -5,7 +5,23 @@ class LogosAdmin(admin.ModelAdmin):
     list_display = ('image',)
 
 class MustPayAdmin(admin.ModelAdmin):
+    list_display = ('name_and_lastname', 'address', 'phone_number', 'job_status')
+
+class RecipientAdmin(admin.ModelAdmin):
     list_display = ('name_and_lastname', 'address', 'phone_number')
+
+class RecipientChildAdmin(admin.ModelAdmin):
+    list_display =('name_and_lastname', 'recipient', 'birthday')
+
+class MustPayReceiptAdmin(admin.ModelAdmin):
+    list_display = ('must_pay', 'full_assessment', 'payment', 'payment_date', 'alimony_percent')
+
+class AlimonyAdmin(admin.ModelAdmin):
+    list_display =('executor_register','must_pay', 'recipient', 'began_paying', 'created_at', 'ruling', 'category', 'user', 'status')
 
 admin.site.register(Logos, LogosAdmin)
 admin.site.register(MustPay, MustPayAdmin)
+admin.site.register(Recipient, RecipientAdmin)
+admin.site.register(RecipientChild, RecipientChildAdmin)
+admin.site.register(MustPayReceipt, MustPayReceiptAdmin)
+admin.site.register(Alimony, AlimonyAdmin)
--- report/models.py
+++ report/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-from decimal import Decimal
-
+from django.contrib.auth.models import User
 class Logos(models.Model):
     image = models.ImageField(upload_to='logo')
 
@@ -8,31 +7,89 @@ class Logos(models.Model):
         verbose_name = ("logo")
         verbose_name_plural = ("logolar")
 
+#algydar
+class Recipient(models.Model):
+    name_and_lastname = models.CharField(verbose_name='Algydaryň ady we familiýasy:', max_length=200, unique=True)
+    birthday = models.DateField(verbose_name='oglan senesi:')
+    phone_number = models.CharField(max_length=12, verbose_name='Telefon belgisi:')
+    address = models.CharField(max_length=200, verbose_name='Öý salgysy:')
+    document_scan = models.FileField(verbose_name='Passport nusgasy:', upload_to="recipient files/", blank=True, null=True)
+
+    def __str__(self):
+        return self.name_and_lastname
+
+    class Meta:
+        verbose_name = "Algydar"
+        verbose_name_plural = "Algydarlar"
+
+# algydaryn chagalary.
+class RecipientChild(models.Model):
+    recipient = models.ForeignKey(Recipient, to_field='name_and_lastname', on_delete=models.CASCADE, verbose_name='Algydar', related_name='children')
+    name_and_lastname = models.CharField(verbose_name='Çaganyň ady we Familiýasy:', max_length=200)
+    birthday = models.DateField(verbose_name='Doglan senesi:')
+    document_scan = models.FileField(verbose_name='Şahadatnama nusgasy', upload_to="recipient child files/", blank=True, null=True)
+
+    def __str__(self):
+        return self.name_and_lastname
+
+    class Meta:
+        verbose_name = "Algydaryň çagasy"
+        verbose_name_plural = "Algydarlaryň çagalary"
+
+# bergidarlar
 class MustPay(models.Model):
-    name_and_lastname = models.CharField(verbose_name='Bergidaryň ady we familiýasy:', max_length=200)
+    name_and_lastname = models.CharField(verbose_name='Bergidaryň ady we familiýasy:', max_length=200, unique=True)
     birthday = models.DateField(verbose_name='Doglan senesi:')
     phone_number = models.CharField(max_length=12, verbose_name='Telefon belgisi 1:')
     phone_number2 = models.CharField(max_length=12, verbose_name='Telefon belgisi 2:', null=True, blank=True) 
     address = models.CharField(max_length=200, verbose_name='Öý salgysy:')
-    document_scan = models.FileField(verbose_name='Passport nusgasy', upload_to=f"files/", blank=True, null=True) #atlyfayl
+    document_scan = models.FileField(verbose_name='Passport nusgasy:', upload_to='must pay files/', blank=True, null=True) #atlyfayl
     job_status = models.CharField(max_length=100, verbose_name='Işleýän ýeri:')
 
-    # recipient = models.ForeignKey(...) bu inniki modele baglanmaly.
-    # alimony = models.ForeignKey(...) munam bashga bir modele baglamakchy bolyan.
-
-    #bu modele slug gerek bolmazmyka diyyan.
-
-    #api lerini ertir chykarmakchy.
     def __str__(self):
         return self.name_and_lastname
-
-    @property
-    def receipt_total(self):
-        total = Decimal(0)
-        for i in self.mustpayreceipt_set.all():
-            total += i.payment
-        return total                                      
 
     class Meta:
         verbose_name = "Bergidar"
         verbose_name_plural = "Bergidarlar"
+
+# tolegler
+class MustPayReceipt(models.Model):
+    must_pay = models.ForeignKey(MustPay, to_field='name_and_lastname', on_delete=models.CASCADE, verbose_name='Bergidar:')
+    full_assessment = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Tölegiň doly möçberi:')
+    payment = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Tölenen möçberi:')
+    payment_date = models.DateField(verbose_name='Iň soňky tölenen senesi:')
+    currency = models.CharField(max_length=20, verbose_name='Walýuta:')
+    document_scan = models.FileField(verbose_name='Tölegi tassyklaýan resminama:', upload_to="must pay receipt files/", blank=True, null=True)
+    alimony_percent = models.IntegerField(verbose_name='alimentiň göterimi:')
+
+    def __str__(self):
+        return self.currency
+
+    class Meta:
+        verbose_name = "Töleg"
+        verbose_name_plural = "Tölegler"
+
+# alimentler.
+class Alimony(models.Model):
+    user = models.ForeignKey(User, verbose_name='Admin:', on_delete=models.SET_NULL, null=True)
+    category = models.CharField(max_length=200, verbose_name='Bölüm:')
+    ruling = models.CharField(verbose_name='Karary çykaran:', max_length=100)
+    ruling_date = models.DateField(verbose_name='Kararyň senesi:')
+    began_paying = models.DateField(verbose_name='Alimenti töläp başlan wagty:')
+    ruling_scan = models.FileField(verbose_name='Kararyň nusgasy:', upload_to="alimony files/", blank=True, null=True)
+    executor = models.CharField(verbose_name='Ýerine ýetirýän:', max_length=100)
+    executor_register = models.CharField(verbose_name='Önumçiligiň belgisi:', max_length=100)
+    executor_date = models.DateTimeField(verbose_name='Önumciligin senesi:')
+    must_pay = models.OneToOneField(MustPay, verbose_name='Bergidaryň ady we familiýasy:', on_delete=models.CASCADE)
+    recipient = models.OneToOneField(Recipient, verbose_name='Algydaryn ady we familiyasy:', on_delete=models.CASCADE)
+    note = models.TextField(verbose_name='Bellik:', blank=True)
+    status = models.BooleanField(verbose_name='Işiň statusy:', default=False)
+    created_at = models.DateTimeField('Işiň döredilen senesi:', auto_now_add=True)
+
+    def __str__(self):
+        return self.category
+
+    class Meta:
+        verbose_name = "Aliment"
+        verbose_name_plural = "Alimentlar"
